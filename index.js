c476a7060d90c6fd0d98c06c5185e2ebff97a094
SowmiyanS <sowmiyan9491@gmail.com>
2025-10-12 19:08:03 +0530

    sync

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ let dt = 10/1000
 let curr_y = bird.offsetTop;
 
 setTimeout(() => {
+    curr_y = bird.offsetTop;
     console.log(curr_y);
     let call_y = curr_y + (g * dt);
     console.log(call_y);
