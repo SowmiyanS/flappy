a5200cff3d773f752b631ff0134aba342a2f11e0
SowmiyanS <sowmiyan9491@gmail.com>
2025-10-12 21:54:12 +0530

    sync

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let g = 20;
 function updateBird() {
     // get the current y position
     let ys = bird.style.top;
-    let y = ys.
+    let y = Numeber(ys.slice(0, -2));
     v = v + g * dt;
     let newY = y + v;
     bird.style.top = newY+"px";
