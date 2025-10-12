e5c20fb3293e93965a02e2cb77f097e83dcb0850
SowmiyanS <sowmiyan9491@gmail.com>
2025-10-12 18:53:56 +0530

    sync

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("flappy");
 
 let bird = document.getElementById("bird");
 
-bird.addEventListener(handleClick);
+bird.addEventListener("click", handleClick);
 
 function handleClick() {
     console.log(this);
