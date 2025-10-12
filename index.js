564b0ba2e06cd2c20f5079873b4cb9a371682f6c
SowmiyanS <sowmiyan9491@gmail.com>
2025-10-12 20:59:16 +0530

    sync

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let bird = document.getElementById("bird");
 // The game loop will run at 30 FPS
 let fps = 30;
 let ticks = 1000/fps;
-let dt = 1/ticks;
+let dt = 1/fps;
 let g = 20;
 
 function updateBird() {
