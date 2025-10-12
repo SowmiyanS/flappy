447e829e144729169a38ee360bdc77bd11fd7c7d
SowmiyanS <sowmiyan9491@gmail.com>
2025-10-12 21:55:34 +0530

    sync

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function updateBird() {
 }
 function tap() {
     // we invert the velocity so the bird now goes in the opposite direction [for a while].
-    v = -g;
+    v = -(g/2);
 }
 function updatePipes() {
     console.log("not implemented");
