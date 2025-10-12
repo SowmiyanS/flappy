d1e0ed0d767bd3f8de83895b2480645c132b42af
SowmiyanS <sowmiyan9491@gmail.com>
2025-10-12 19:53:00 +0530

    sync

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 console.log("flappy");
 
 let bird = document.getElementById("bird");
-let ticks = 30/1000;
+// The game loop will run at 30 FPS
+let fps = 30;
+let ticks = fps/1000;
 
 function loop() {
 
